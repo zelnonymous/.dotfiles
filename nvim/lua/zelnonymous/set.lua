f96d0e5e0be1024b265ccbadf5c1970229652e7c
Aaron Brown <zelnot@gmail.com>
2024-01-12 16:27:00 -0500

    Re-enable nvim ignorcase binding, increase command timeout, add undofile with platform specific pathing.

--- nvim/lua/zelnonymous/set.lua
+++ nvim/lua/zelnonymous/set.lua
@@ -1,13 +1,10 @@
-
--- disable nvim intro
 vim.opt.shortmess:append "sI"
 
 vim.opt.signcolumn = "yes"
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 vim.opt.termguicolors = true
-vim.opt.timeoutlen = 400
-vim.opt.undofile = tru
+vim.opt.timeoutlen = 1000
 
 vim.opt.guicursor = ""
 
@@ -25,16 +22,19 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
---vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
---vim.opt.undofile = true
+if vim.fn.has("WIN32") or vim.vn.has("WIN64") then
+    vim.opt.undodir = os.getenv("HOMEDRIVE").. "\\" 
+        .. os.getenv("HOMEPATH") ..
+        "\\vim-undo"
+else
+    vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+end
+vim.opt.undofile = true
 
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
 
-vim.opt.termguicolors = true
-
 vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
